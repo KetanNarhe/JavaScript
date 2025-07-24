5b0bd4a1959a740ea88f00281460587e256f2fc7
Ketan narhe <ketannarhe02@gmail.com>
2025-07-24 06:31:00 -0700

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,6 @@
+// const aletResult=alert("Your message goes Here");
+
+// const isConfirmed=confirm("Would you like to proceed");
+
+const UserInput= prompt("Plese enter your Name");
+console.log(UserInput);
