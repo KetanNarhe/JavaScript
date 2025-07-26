b57d3009799f509ad37f0d6e7d2a734c27f2817a
Ketan narhe <ketannarhe02@gmail.com>
2025-07-26 08:40:19 -0700

    Add files via upload

---
+++ while.js
@@ -0,0 +1,22 @@
+
+let num=0;
+
+console.log('Program stated');
+
+// while(num<10)
+// {
+//       console.log(num+1);
+//     num=num+1;
+// }
+
+let frinds=['ketan','prashant','rushi','macchu','akshay'];
+
+let i=0;
+while(i<5)
+{
+    
+    console.log(frinds[i]+" ");
+    i++;
+}
+
+console.log('Program ended');
