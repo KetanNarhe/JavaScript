ffbb03d739c60aa4f17fdf76c0c1027812c0175d
Ketan narhe <ketannarhe02@gmail.com>
2025-07-26 22:48:17 -0700

    Add files via upload

---
+++ object.js
@@ -0,0 +1,24 @@
+//create object using literals
+const user={
+
+  firstName:"ketan",
+  lastName:"Narhe",
+  age:23,
+
+  Address:{
+
+    city:"Pune",
+    pincode:"4100", 
+    state:"Maharashtra",
+
+    Population:{
+
+        popOfState:"14596857457474840",
+        popOfCity:"95958574736367474",
+    }
+  }
+  
+
+
+};
+console.log(user);
