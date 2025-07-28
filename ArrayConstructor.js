855c49cc6d8e973421ce5c97ab429be8942ed8aa
Ketan narhe <ketannarhe02@gmail.com>
2025-07-28 09:14:46 -0700

    Add files via upload

---
+++ ArrayConstructor.js
@@ -0,0 +1,49 @@
+// const fruits=new Array("Apple" ,"mango","Santra","stober");
+// console.log(fruits);
+
+const num1=[1,2,3,4,5];
+const num2=[6,7,8,9,10];
+
+
+//Concat method
+//const addNumber=num1.concat(num2);
+//console.log(addNumber);
+
+// push method
+//console.log(addNumber.push());
+
+//shipt method..
+//console.log(addNumber.shift());
+
+//unshipt method...
+// console.log(addNumber.unshift());
+//console.log(num1(2));
+
+
+//includes methos
+//console.log(Num1.includes(2));
+
+
+//sort  Method().....
+//const user=["Dnyanuu","Akshay","ketan","Macchindra","Rupali"]
+//document.write(user.sort()+" ");
+// console.log(user.reverse());
+
+//slice  method()....
+// const user=["Dnyanuu","Akshay","ketan","Macchindra","Rupali"]
+// document.write(user.slice(0,3)+" ");
+// document.write(user.slice(0,4)+" ");
+// console.log(user.slice(1,3));
+
+
+
+const user=["Dnyanuu","Akshay","ketan","Macchindra","Rupali"]
+//document.write(user.splice(0,2)+" ");
+//document.write(user.splice(0,2)+" ");
+
+//add Element
+const user1=["ketan","Macchindra","Rupali"]
+document.write(user1.splice(2,3,4,)+" ");
+
+
+
