8b6c580ec175d89b56da04353967db140629f7ac
Ketan narhe <ketannarhe02@gmail.com>
2025-07-29 03:35:17 -0700

    Add files via upload

---
+++ MergeArray.js
@@ -0,0 +1,13 @@
+let arr1 = [1, 3, 5];
+let arr2 = [2, 4, 6];
+
+ const merged=arr1.concat(arr2);
+
+console.log("Merged:"+merged);
+
+//merged two Array without concat with sorting 
+// const merges=[...arr1,...arr2];
+
+// merges.sort();
+
+// console.log(merges);
